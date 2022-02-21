95f771c5f84e303df53595436f1866bec822f309
abhinavagarwal-web <62370646+abhinavagarwal-web@users.noreply.github.com>
2022-02-21 16:48:46 +0530

    Add files via upload

---
+++ react shooping cart/src/component/products.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: 1,
+    title: "Iphone 13",
+    description: "white  in color",
+    price: "25000",
+    img: "https://www.91-img.com/gallery_images_uploads/d/f/dfa4b5c0995ed2af9c47a9d173792fd176867bad.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "iphone x",
+    description: "gold in color",
+    price: "125000",
+    img: "https://th.bing.com/th/id/OIP.yTzLjjx_cjYvXrj_G2Bj4QHaHa?pid=ImgDet&rs=1",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Samsung A10",
+    description: "black in color",
+    price: "900000",
+    img: "https://www.91-img.com/gallery_images_uploads/5/b/5bbcecdcf73b7093c0adcd91fa411f05e8eb0960.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "sony",
+    description: "black in color",
+    price: "50000",
+    img: "https://th.bing.com/th/id/OIP.yv9nn61KJpxI9NS9IxHy3AHaHM?pid=ImgDet&rs=1",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "one plus nord",
+    description: "black in color",
+    price: "75000",
+    img: "https://images.idgesg.net/images/article/2020/07/oneplus-nord-100852438-large.jpg",
+    amount: 1,
+  },
+];
